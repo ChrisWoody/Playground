b969fff2dff6b0a84646e0cc13fbf8eec5a77040
Chris Wood <chris.woody@live.com.au>
2017-09-09 12:45:01 +0800

    added a 'text to upper case' function ready for webjob/signalr work

---
+++ src/Playground.WebSite/Controllers/FunctionsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Playground.WebSite.Controllers
+{
+    public class FunctionsController : Controller
+    {
+        public IActionResult TextToUpperCase()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async void PerformTextToUpperCase(string text)
+        {
+            // send request via queue storage
+        }
+    }
+}
