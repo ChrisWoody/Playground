56b422fac00a0d7d0269f84d042e8c6fb4524046
Chris Wood <chris.woody@live.com.au>
2017-09-10 11:08:20 +0800

    added basic function to perform 'upper case', and in lieu of getting dashboard logging working (and because it's cool) push the 'log' to another queue

--- src/Playground.WebJob/Functions.cs
+++ src/Playground.WebJob/Functions.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.Azure.WebJobs;
+using Playground.Core.Messages;
 
 namespace Playground.WebJob
 {
     public class Functions
     {
-        // This function will get triggered/executed when a new message is written 
-        // on an Azure Queue called queue.
-        public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        public static void TextToUpperCase([QueueTrigger("testqueue")] TextToUpperCaseMessage message, [Queue("testhandled")] out string response, TextWriter log)
         {
-            log.WriteLine(message);
+            response = message.Text.ToUpper();
         }
     }
 }
